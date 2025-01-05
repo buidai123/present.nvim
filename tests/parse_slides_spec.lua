80f237bb2dc29fa647d80f46a8e5a6a72f38bd2e
Bui Dai <ss1280dzzz@gmail.com>
2025-01-05 10:48:22 +0700

    test: add test for parsing codeblocks

--- tests/parse_slides_spec.lua
+++ tests/parse_slides_spec.lua
@@ -15,6 +15,7 @@ describe("present.parse_slides", function()
           {
             title = "# This is the first slide",
             body = { "this is the body" },
+            blocks = {},
           },
         },
       },
@@ -23,5 +24,54 @@ describe("present.parse_slides", function()
         "this is the body",
       })
     )
+  end)
+  it("should parse a file with one slide having codeblocks", function()
+    local results = parse({
+      "# This is the first slide",
+      "this is the body",
+      "```lua",
+      "print('Hello World!')",
+      "```",
+      " ",
+      "```python",
+      "name = 'Bob'",
+      "print(f'Hello World! {name}')",
+      "```",
+    })
+
+    eq(1, #results.slides)
+    local slide = results.slides[1]
+
+    eq("# This is the first slide", slide.title)
+
+    eq({
+      "this is the body",
+      "```lua",
+      "print('Hello World!')",
+      "```",
+      " ",
+      "```python",
+      "name = 'Bob'",
+      "print(f'Hello World! {name}')",
+      "```",
+    }, slide.body)
+
+    local blocks = {
+      {
+        language = "lua",
+        body = vim.trim([[
+print('Hello World!')
+        ]]),
+      },
+      {
+        language = "python",
+        body = vim.trim([[
+name = 'Bob'
+print(f'Hello World! {name}')
+        ]]),
+      },
+    }
+
+    eq(blocks, slide.blocks)
   end)
 end)
